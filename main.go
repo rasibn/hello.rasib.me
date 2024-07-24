2e32493ae11f6561ba1a78a69cf4f4d9fa8df64f
Rasib Nadeem <rasibnadeem101@gmail.com>
2024-07-25 03:53:54 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// helloHandler handles HTTP requests to the "/hello" endpoint
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Set the Content-Type header to text/plain
+	w.Header().Set("Content-Type", "text/plain")
+	// Write "Hello, World!" to the response
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	// Create a new ServeMux (router)
+	mux := http.NewServeMux()
+
+	// Register the helloHandler function to handle requests to the "/hello" path
+	mux.HandleFunc("GET /", helloHandler)
+
+	// Start the HTTP server on port 8080
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
